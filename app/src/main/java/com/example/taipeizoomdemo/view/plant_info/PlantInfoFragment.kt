466d2f32a6fb12cb727637764ad26cdd4dd0cede
yaerse <yaerse@yahoo.com.tw>
2021-05-29 14:51:26 +0800

    [新增] 完成植物資訊頁面的呈現與點擊返回鍵的功能

--- app/src/main/java/com/example/taipeizoomdemo/view/plant_info/PlantInfoFragment.kt
+++ app/src/main/java/com/example/taipeizoomdemo/view/plant_info/PlantInfoFragment.kt
@@ -1,12 +1,14 @@
 package com.example.taipeizoomdemo.view.plant_info
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.taipeizoomdemo.R
 import com.example.taipeizoomdemo.databinding.FragmentPlantInfoBinding
 import com.example.taipeizoomdemo.model.repository.PlantInfoRepository
 import com.example.taipeizoomdemo.view.base.BaseFragment
@@ -22,7 +24,32 @@ class PlantInfoFragment : BaseFragment<PlantInfoViewModel, FragmentPlantInfoBind
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Log.d("JLin", "plant name: ${plantPojo.nameCh}")
+        setView()
+        setListener()
+    }
+
+    private fun setView() {
+        binding.title.text = plantPojo.nameCh
+        binding.nameCh.text = plantPojo.nameCh
+        binding.nameEn.text = plantPojo.nameEn
+        binding.alias.text = plantPojo.nameAlias
+        binding.brief.text = plantPojo.brief
+        binding.feature.text = plantPojo.feature
+        binding.application.text = plantPojo.application
+        binding.lastUpdate.text = String.format(
+            getString(R.string.plant_info_last_update), plantPojo.lastUpdate)
+
+        Glide.with(activity).load(plantPojo.picUrl).into(binding.image)
+    }
+
+    private fun setListener() {
+        binding.back.setOnClickListener {
+            closeThePage()
+        }
+    }
+
+    private fun closeThePage() {
+        findNavController().popBackStack()
     }
 
     override fun getViewModel() = PlantInfoViewModel::class.java
